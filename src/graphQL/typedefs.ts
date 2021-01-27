b5c09601a14f65ba20276686ef9148ffc18e8f8e
junior08 <zimkhandiaye@gmail.com>
2021-01-27 11:36:33 +0000

    mis a jour du fichier typedefs

--- src/graphQL/typedefs.ts
+++ src/graphQL/typedefs.ts
@@ -5,12 +5,28 @@ export default gql `
 scalar Date;
    type Subscription {
     id : ID!
-
+    name: String!
+    price: Number!
+    dure: Date!
+    createdAt: Date
+    updatedAt: Date
+   }
+   type Payment{
+    status:Boolean,
+    date_payment: Date
+    date_created: Date
    }
 
    type Subscriber {
     id : ID!
+    date_start: Date
+    date_end: Date
+    status: Boolean!
+    settings: Settings
+    payments: [Payment]
    }
+
+ 
 
    type Settings {
     id : ID!
--- src/model/Subscriber.ts
+++ src/model/Subscriber.ts
@@ -6,7 +6,15 @@ const SubscriberSchema = new Schema({
   subcription : {type: Schema.Types.ObjectId, ref : 'Subscription'},
   settings : {type: Schema.Types.ObjectId, ref : 'Settings'},
   date_start: {type : Date},
-  date_end: {type: Date}
+  date_end: {type: Date},
+  status: {type: Boolean},
+  payments: [
+    {
+      status: {type: Boolean},
+      date_payment: {type: Date},
+      date_created: {type: Date}
+    }
+  ]
 }, {timestamps: true});
 
 
